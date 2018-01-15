c21ac6e3cb60674133ccf9ff3d12dd686c8637ea
eyes_only <mailz.eyes.only@gmail.com>
2018-01-15 14:17:14 +0300

    getopt option parser is used

--- gen-qr-pass.sh
+++ gen-qr-pass.sh
@@ -1,14 +1,50 @@
 #!/usr/bin/env sh
-length=$1
-# Check if special chars required
-if [[ "$2" == "strong" ]]; then
-pwgen -sync $length 1 | qrencode -s 10 -o qrpass.png
-display qrpass.png
-shred -v -n 30 -u qrpass.png
-echo "Done."
+
+OPT=$(getopt -o skh --long strong,help,keep -n 'gen-qr-pass.sh' -- "$@")
+if [ $? != 0 ] ; then echo "script usage: $(basename $0) [ -s --strong (strong password with special characters)] [-h --help ] [-k --keep (keep password on the output)] [ password_length ]" >&2 ; exit 1 ; fi
+eval set -- "$OPT"
+STRONG=false
+USAGE=false
+KEEP=false
+while true ; do
+    case "$1" in
+    -s|--strong)
+      STRONG=true ; shift
+    ;;
+
+    -h|--help)
+      USAGE=true ; shift
+    ;;
+
+    -k|--keep)
+      KEEP=true ; shift
+    ;;
+
+    --) shift; break ;;
+
+    *) break ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+LENGTH="$*"
+if [ "$*" == "" ] ; then echo "script usage: $(basename $0) [ -s --strong (strong password with special characters)] [-h --help ] [-k --keep (keep password on the output)] [ password_length ]" >&2 ; exit 1 ; fi
+if [[ "$USAGE" = false ]]; then
+if [[ "$LENGTH" -gt 0 && "$STRONG" = true ]] ;then
+PASSWORD=$(pwgen -sync $LENGTH 1)
+qrencode -s 10 "$PASSWORD" -o qrpass.png
 else
-pwgen -snc $length 1 | qrencode -s 10 -o qrpass.png
+if [[ "$LENGTH" -gt 0 && "$STRONG" = false ]] ;then
+PASSWORD=$(pwgen -snc $LENGTH 1)
+qrencode -s 10 "$PASSWORD" -o qrpass.png
+fi
+fi
+if [[ "$KEEP" = true ]]; then
+echo "$PASSWORD"
+fi
 display qrpass.png
-shred -v -n 30 -u qrpass.png
-echo "Done."
+shred -n 30 -u qrpass.png
+echo "Done"
+else
+echo "script usage: $(basename $0) [ -s --strong (strong password with special characters)] [-h --help ] [-k --keep (keep password on the output)] [ password_length ]" >&2
+exit 1
 fi
