57a611d4b9407d00096a8038548af424243c4483
eyes_only <mailz.eyes.only@gmail.com>
2018-01-19 15:59:34 +0300

    Сhanged the output of the password length error

--- gen-qr-pass.sh
+++ gen-qr-pass.sh
@@ -26,19 +26,19 @@ while true ; do
   esac
 done
 shift "$(($OPTIND -1))"
-if [[ "$USAGE" = false ]]; then
-if [ "$*" == "" ] ; then echo "script usage: $(basename $0) [ -s --strong (strong password with special characters)] [-h --help ] [-k --keep (keep password on the output)] [ password_length ]" >&2 ; exit 1 ; fi
+if [[ $USAGE = false ]]; then
+if [ "$*" == "" ] ; then echo -e "You did not specify a password length! \nscript usage: $(basename $0) [ -s --strong (strong password with special characters)] [-h --help ] [-k --keep (keep password on the output)] [ password_length ]" >&2 ; exit 1 ; fi
 LENGTH="$*"
-if [[ "$STRONG" = true ]] ;then
+if [[ $STRONG = true ]] ;then
 PASSWORD=$(pwgen -sync $LENGTH 1)
 qrencode -s 10 "$PASSWORD" -o qrpass.png
 else
-if [[ "$STRONG" = false ]] ;then
+if [[ $STRONG = false ]] ;then
 PASSWORD=$(pwgen -snc $LENGTH 1)
 qrencode -s 10 "$PASSWORD" -o qrpass.png
 fi
 fi
-if [[ "$KEEP" = true ]]; then
+if [[ $KEEP = true ]]; then
 echo "$PASSWORD"
 fi
 display qrpass.png
