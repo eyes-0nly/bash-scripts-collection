937755f55a05dcb41b5a53c8806765b6bae69973
eyes_only <mailz.eyes.only@gmail.com>
2018-02-12 10:26:02 +0300

    added time to backup name pattern

--- backup-pg.sh
+++ backup-pg.sh
@@ -4,11 +4,11 @@ mount /dev/sda6 /mnt/BACKUPS # change partition name to yours
 FILES=$(find . -maxdepth 1 -type f | wc -l)
 if [ "$FILES" -lt "30" ] # number of backup files
 	then
-		slug=$(date +"%m_%d_%Y")
+		slug=$(date +"%T_%d_%m_%Y")
 		sudo -u postgres pg_dumpall | gzip > "/mnt/BACKUPS/dumpall_postgres/dumpall_$slug.gz" # change to your path
 	else
 		rm -rf $(find . -maxdepth 1 -type f -printf '%T+ %p\n' | sort | head -n 1) # remove oldest backup file if their number is exceeded
-		slug=$(date +"%m_%d_%Y")
+		slug=$(date +"%T_%d_%m_%Y")
                 sudo -u postgres pg_dumpall | gzip > "/mnt/BACKUPS/dumpall_postgres/dumpall_$slug.gz" # change to your path
 fi
 umount /dev/sda6 # change partition name to yours
