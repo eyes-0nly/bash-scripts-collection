bfb442d51a90fc4bb3b367c6f92090017ee2be07
eyes_only <mailz.eyes.only@gmail.com>
2018-12-26 18:10:07 +0300

    Just a few fixes

--- backup-pg.sh
+++ backup-pg.sh
@@ -2,13 +2,13 @@
 
 mount /dev/sda6 /mnt/BACKUPS # change partition name to yours
 AV_SPACE=$(df -Ph /mnt/BACKUPS | tail -1 |awk '{gsub ( "[G]","" ) ; print $4}')
-if [ "$AV_SPACE" -gt "18" ] # check available space and if it greater than 18G do backup without removing one old buckup file
+if [ ($(echo "$AV_SPACE < 18" | bc) -eq 1 ] # check available space and if it greater than 18G do backup without removing one old buckup file
 	then
 		slug=$(date +"%T_%d_%m_%Y")
 		sudo -u postgres psql -c '\l' > "/mnt/BACKUPS/dumpall_postgres/list_$slug" #list of all databases
 		sudo -u postgres pg_dumpall | gzip > "/mnt/BACKUPS/dumpall_postgres/dumpall_$slug.gz" # change to your path
 	else
-		rm -rf $(find . -name "dumpall_*" -type f -printf '%T@+ %p\n' | sort -n | head -n 1) # remove oldest backup file if their number is exceeded
+		rm -f $(find . -name "dumpall_*" -type f -printf '%T@+ %p\n' | sort -n | head -n 1) # remove oldest backup file if their number is exceeded
 		slug=$(date +"%T_%d_%m_%Y")
 		sudo -u postgres psql -c '\l' > "/mnt/BACKUPS/dumpall_postgres/list_$slug"
                 sudo -u postgres pg_dumpall | gzip > "/mnt/BACKUPS/dumpall_postgres/dumpall_$slug.gz" # change to your path
